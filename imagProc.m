77d1d5478c56a3ead1e17d08054a08625f494293
Thomas Gregov <thomasgregov@gmail.com>
2019-04-21 13:46:14 +0200

    Improvements

--- imagProc.m
+++ imagProc.m
@@ -26,6 +26,8 @@ videoResultBefore = imabsdiff(videoMedians, videoGS);
 videoResultAfter = imabsdiff(videoMedians, videoGS);
 
 %% Before pico-injection
+fprintf(['\nPart I - Before pico-injection\n']);
+fprintf(['------------------------------\n']);
 % Adjust contrast
 % c = 70;
 % f = figure('Name', ...
@@ -97,7 +99,7 @@ img = videoResultBefore(:, :, int64(sizeV(4)/2));
 while(true)
     imgTemp = bwareaopen(img, part);
     imshow(imgTemp);
-    textPart = text(50, 50, ['$Size\ particules = $ ', num2str(part)], ...
+    textPart = text(50, 50, ['$A = $ ', num2str(part)], ...
         'Color', 'Red', 'FontSize', 20);
     set(textPart, 'interpreter', 'latex');
     
@@ -123,7 +125,7 @@ end
 
 % Choose window
 f = figure('Name', 'Image processing: scaling before pico-injection');
-imshow(videoGS(:, :, int64(sizeV(4)/2)));
+imshow(videoGS(:, :, 1));
 while(true)
     [xB, yB] = ginputc(2, 'Color', 'b', 'LineWidth', 1);
     xB = double(int64(xB));
@@ -178,12 +180,14 @@ for i = 1:sizeV(4)
 end
 
 % Remove unprobable results (quite arbitrary...)
-LBefore = LBefore(LBefore > mean(LBefore)/2);
+LBefore = LBefore(LBefore > mean(LBefore)*0.3);
 disp([num2str(length(LBefore)), ...
     ' droplets were found before pico-injection.']);
 
 
 %% After pico-injection
+fprintf(['\nPart II - After pico-injection\n']);
+fprintf(['------------------------------\n']);
 % Adjust contrast
 % c = 70;
 % f = figure('Name', ...
@@ -255,7 +259,7 @@ img = videoResultAfter(:, :, int64(sizeV(4)/2));
 while(true)
     imgTemp = bwareaopen(img, part);
     imshow(imgTemp);
-    textPart = text(50, 50, ['$Size\ particules = $ ', num2str(part)], ...
+    textPart = text(50, 50, ['$A = $ ', num2str(part)], ...
         'Color', 'Red', 'FontSize', 20);
     set(textPart, 'interpreter', 'latex');
     
@@ -281,9 +285,7 @@ end
 
 % Choose window
 f = figure('Name', 'Image processing: scaling after pico-injection');
-imshow(videoGS(:, :, int64(sizeV(4)/2)));
-hB = rectangle('Position', [xB(1), yB(1), ...
-    abs(xB(2)-xB(1)), abs(yB(2)-yB(1))], 'EdgeColor', 'Blue');
+imshow(videoGS(:, :, 1));
 while(true)
     [xA, yA] = ginputc(2, 'Color', 'b', 'LineWidth', 1);
     xA = double(int64(xA));
@@ -299,11 +301,6 @@ while(true)
     end
 end
 close(f);
-
-disp('Click on enter');
-pause;
-prompt = 'How many drops between the two rectangles? ';
-numOfDrops = input(prompt);
 
 videoCroppedAfter = zeros(int64(abs(yA(2)-yA(1))) + 1, ...
     int64(abs(xA(2)-xA(1))) + 1, sizeV(4));
@@ -342,27 +339,59 @@ for i = 1:sizeV(4)
 end
 
 % Remove unprobable results (quite arbitrary...)
-LAfter = LAfter(LAfter > mean(LAfter)/2);
+LAfter = LAfter(LAfter > mean(LAfter)*0.3);
 disp([num2str(length(LAfter)), ...
     ' droplets were found after pico-injection.']);
 
-% Change of length due to pico-injection
-str = 'Not the same number of drops before and after pico-injection';
-enoughDrop = false;
-if length(LBefore) == length(LAfter)
-    if length(LBefore) > numOfDrops+1
-        enoughDrop = true;
-        diffL = LAfter(numOfDrops+1:end) - LBefore(1:end-numOfDrops);
-    else
-        fprintf('Not enough drops detected');
+%% Compare before and after pico-injection
+fprintf('\nPart III - Comparison\n');
+fprintf('------------------------------\n');
+fprintf(2, ['<strong>/!\ We assume that the pico-injeciton process is', ...
+    ' perfectly working.</strong>\n']);
+f = figure('Name', ...
+    ['Image processing: Number of drops between rectangles ?', ...
+    ' <Press enter>']);
+imshow(videoGS(:, :, 1));
+hB = rectangle('Position', [xB(1), yB(1), ...
+    abs(xB(2)-xB(1)), abs(yB(2)-yB(1))], 'EdgeColor', 'Red');
+hA = rectangle('Position', [xA(1), yA(1), ...
+    abs(xA(2)-xA(1)), abs(yA(2)-yA(1))], 'EdgeColor', 'Red');
+while(true)
+    pause;
+    currkey = get(gcf,'CurrentKey');
+    if strcmp(currkey, 'return')
+        break;
     end
-else
-    fprintf(2, ['<strong>WARNING: ', str, '</strong>\n']);
 end
+close(f);
 
+prompt = 'How many droplets between the two rectangles? ';
+numOfDrops = input(prompt);
+
+nDropletsComp = min(length(LBefore) - numOfDrops - 1, ...
+    length(LAfter) - numOfDrops - 1);
+disp([num2str(nDropletsComp), ' droplets were used for ', ...
+    'the before/after pico-injection comparison.']);
+LComp = LAfter(numOfDrops+1:numOfDrops+nDropletsComp) ...
+        - LBefore(1:nDropletsComp);
+
+% % Change of length due to pico-injection
+% str = 'Not the same number of droplets before and after pico-injection.';
+% enoughDrop = false;
+% if length(LBefore) == length(LAfter)
+%     if length(LBefore) > numOfDrops+1
+%         enoughDrop = true;
+%         diffL = LAfter(numOfDrops+1:end) - LBefore(1:end-numOfDrops);
+%     else
+%         fprintf('Not enough drops detected');
+%     end
+% else
+%     fprintf(2, ['<strong>WARNING: ', str, '</strong>\n']);
+% end
 
 %% Display the results
-
+fprintf('\nPart IV - Results\n');
+fprintf('------------------------------\n');
 % Before pico-injection
 figure('Name', 'Length of the droplets before pico-injection');
 subplot(1, 2, 1);
@@ -382,6 +411,10 @@ set(gca, 'Units', 'normalized', 'FontUnits', 'points', 'FontWeight', ...
     'normal', 'FontSize', 15, 'FontName','Times');
 ylabel('$L$ [pix]', 'interpreter', 'latex');
 % ylim([0, 2*max(LBefore)]);
+
+disp('Before pico-injection:');
+disp(['   - mean(L) = ', num2str(mean(LBefore)), ' pix']);
+disp(['   - std(L) = ', num2str(std(LBefore)), ' pix']);
 
 % After pico-injection
 figure('Name', 'Length of the droplets after pico-injection');
@@ -403,24 +436,51 @@ set(gca, 'Units', 'normalized', 'FontUnits', 'points', 'FontWeight', ...
 ylabel('$L$ [pix]', 'interpreter', 'latex');
 % ylim([0, 2*max(LAfter)]);
 
-% Change of length
-if(enoughDrop)
-    figure('Name', 'Change of Length due to pico-injection');
-    subplot(1, 2, 1);
-    histogram(diffL, 'Normalization', 'pdf');
-    % hist(diffL, sqrt(length(diffL)), 'Normalization', 'pdf');
-    grid on;
-    set(gca, 'Units', 'normalized', 'FontUnits', 'points', ...
-        'FontWeight', 'normal', 'FontSize', 15, 'FontName','Times');
-    xlabel('$\Delta L$ [pix]', 'interpreter', 'latex');
-    ylabel('PDF [-]', 'interpreter', 'latex');
-    xlim([0, 2*max(diffL)]);
+disp('After pico-injection:');
+disp(['   - mean(L) = ', num2str(mean(LAfter)), ' pix']);
+disp(['   - std(L) = ', num2str(std(LAfter)), ' pix']);
 
-    subplot(1, 2, 2);
-    boxplot(diffL);
-    grid on;
-    set(gca, 'Units', 'normalized', 'FontUnits', 'points', ...
-        'FontWeight', 'normal', 'FontSize', 15, 'FontName','Times');
-    ylabel('$\Delta L$ [pix]', 'interpreter', 'latex');
-    % ylim([0, 2*max(diffL)]);
-end
+% Comparison before/after
+figure('Name', 'Added length of the droplets before/after pico-injection');
+subplot(1, 2, 1);
+histogram(LComp, 'Normalization', 'pdf');
+% hist(Lafter, sqrt(length(LAfter)), 'Normalization', 'pdf');
+grid on;
+set(gca, 'Units', 'normalized', 'FontUnits', 'points', 'FontWeight', ...
+    'normal', 'FontSize', 15, 'FontName','Times');
+xlabel('$\Delta L$ [pix]', 'interpreter', 'latex');
+ylabel('PDF [-]', 'interpreter', 'latex');
+xlim([0, 2*max(LComp)]);
+
+subplot(1, 2, 2);
+boxplot(LComp);
+grid on;
+set(gca, 'Units', 'normalized', 'FontUnits', 'points', 'FontWeight', ...
+    'normal', 'FontSize', 15, 'FontName','Times');
+ylabel('$\Delta L$ [pix]', 'interpreter', 'latex');
+
+disp('Comparison before/after pico-injection:');
+disp(['   - mean(LComp) = ', num2str(mean(LComp)), ' pix']);
+disp(['   - std(LComp) = ', num2str(std(LComp)), ' pix']);
+
+% Change of length
+% if(enoughDrop)
+%     figure('Name', 'Change of Length due to pico-injection');
+%     subplot(1, 2, 1);
+%     histogram(diffL, 'Normalization', 'pdf');
+%     % hist(diffL, sqrt(length(diffL)), 'Normalization', 'pdf');
+%     grid on;
+%     set(gca, 'Units', 'normalized', 'FontUnits', 'points', ...
+%         'FontWeight', 'normal', 'FontSize', 15, 'FontName','Times');
+%     xlabel('$\Delta L$ [pix]', 'interpreter', 'latex');
+%     ylabel('PDF [-]', 'interpreter', 'latex');
+%     xlim([0, 2*max(diffL)]);
+% 
+%     subplot(1, 2, 2);
+%     boxplot(diffL);
+%     grid on;
+%     set(gca, 'Units', 'normalized', 'FontUnits', 'points', ...
+%         'FontWeight', 'normal', 'FontSize', 15, 'FontName','Times');
+%     ylabel('$\Delta L$ [pix]', 'interpreter', 'latex');
+%     % ylim([0, 2*max(diffL)]);
+% end
